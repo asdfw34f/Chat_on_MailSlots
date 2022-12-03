b8709de39ca8942712b1cc37b736f50243f8fdc1
Daniil Demekhin <daniil2002.d11@gmail.com>
2022-12-04 00:13:02 +0300

    Добавьте файлы проекта.

---
+++ MailClient/MailClient.cpp
@@ -0,0 +1,56 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+LPCTSTR SlotName = TEXT("\\\\.\\mailslot\\sample_mailslot");
+
+BOOL WriteSlot(HANDLE hSlot, LPCTSTR lpszMessage)
+{
+    BOOL fResult;
+    DWORD cbWritten;
+
+    fResult = WriteFile(hSlot,
+        lpszMessage,
+        (DWORD)(lstrlen(lpszMessage) + 1) * sizeof(TCHAR),
+        &cbWritten,
+        (LPOVERLAPPED)NULL);
+
+    if (!fResult)
+    {
+        printf("WriteFile failed with %d.\n", GetLastError());
+        return FALSE;
+    }
+
+    printf("Slot written to successfully.\n");
+
+    return TRUE;
+}
+
+int main()
+{
+    HANDLE hFile;
+
+    hFile = CreateFile(SlotName,
+        GENERIC_WRITE,
+        FILE_SHARE_READ,
+        (LPSECURITY_ATTRIBUTES)NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        (HANDLE)NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateFile failed with %d.\n", GetLastError());
+        return FALSE;
+    }
+
+    WriteSlot(hFile, TEXT("Message one for mailslot."));
+    WriteSlot(hFile, TEXT("Message two for mailslot."));
+
+    Sleep(5000);
+
+    WriteSlot(hFile, TEXT("Message three for mailslot."));
+
+    CloseHandle(hFile);
+
+    return TRUE;
+}
